1223f10f3eff18b57600f184ebad0cf61631696f
Sebastien Boslimon <sboslimon@yahoo.fr>
2021-03-28 22:44:14 +0900

    cosmetics in bubblesort part 2

--- bubblesort.py
+++ bubblesort.py
@@ -1,9 +1,9 @@
 
 def bubbleSort(list_to_sort, dec=0):
     length = len(list_to_sort)
-    for i in range(0,length-1): 
+    for i in range(0, length-1):
         for j in range(0, length-i-1): 
-            if list_to_sort[j] > list_to_sort[j+1] :
+            if list_to_sort[j] > list_to_sort[j+1]:
                 list_to_sort[j], list_to_sort[j+1] = list_to_sort[j+1], list_to_sort[j]
 
     if dec:
