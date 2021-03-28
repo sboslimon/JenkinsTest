6cd2e373c262213e2df8b4b5b5c94c689fb23d6b
Sebastien Boslimon <sboslimon@yahoo.fr>
2021-03-28 22:37:52 +0900

    cosmetics in bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,14 +1,14 @@
 
-def bubbleSort(list, dec=0): 
-    length = len(list)
+def bubbleSort(list_to_sort, dec=0):
+    length = len(list_to_sort)
     for i in range(0,length-1): 
         for j in range(0, length-i-1): 
-            if list[j] > list[j+1] : 
-                list[j], list[j+1] = list[j+1], list[j] 
+            if list_to_sort[j] > list_to_sort[j+1] :
+                list_to_sort[j], list_to_sort[j+1] = list_to_sort[j+1], list_to_sort[j]
 
     if dec:
-        list.reverse()
-    return list
+        list_to_sort.reverse()
+    return list_to_sort
 
 
 
